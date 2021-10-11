5e95b2d4be72d0b2c60e940627f715a149dac8cb
Morgan Byrne <morganlbyrne@gmail.com>
2021-10-11 14:38:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+//Code your solutions in this file
+
+function writeCards(strArray, event) {
+    let newArray = [];
+    for (let i = 0; i < strArray.length; i++) {
+        newArray.push(`Thank you, ${strArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(integer) {
+    let counter = integer;
+    while (counter >= 0) {
+        console.log(counter);
+        counter--;
+    } 
+}
+
+
