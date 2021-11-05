13c6ff7420fa33146e5326dded5c9ed13f0640b9
Morgan <morganlbyrne@gmail.com>
2021-11-05 11:50:17 -0400

    commit tree f3c7eb640665a115419a68f3d7abc75c5bfa3356

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 //Code your solutions in this file
 
-function writeCards(strArray, event) {
-    let newArray = [];
-    for (let i = 0; i < strArray.length; i++) {
-        newArray.push(`Thank you, ${strArray[i]}, for the wonderful ${event} gift!`);
+// for (let age = 30; age < 40; age++) {
+//     debugger;
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+
+// const names = ["teddy bear", "drone", "doll"];
+
+function writeCards(names, event){
+    let cardArray = [];
+    for (let i = 0; i < names.length; i++) {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-    return newArray;
+    return cardArray;
 }
 
+writeCards(names, 'bday');
+
 function countDown(integer) {
-    let counter = integer;
-    while (counter >= 0) {
-        console.log(counter);
-        counter--;
-    } 
+    let count = integer;
+    while (count >=0) {
+        console.log(count--);
+        debugger;
+    }
 }
+
+countDown(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(strArray, event) {
+//     let newArray = [];
+//     for (let i = 0; i < strArray.length; i++) {
+//         newArray.push(`Thank you, ${strArray[i]}, for the wonderful ${event} gift!`);
+//     }
+//     return newArray;
+// }
+
+// function countDown(integer) {
+//     let counter = integer;
+//     while (counter >= 0) {
+//         console.log(counter);
+//         counter--;
+//     } 
+// }
 
 
